ee30ef083a1e2d7269bcc2666f7a21185f983e00
Frank <frank@smartman.com.tw>
2025-02-14 15:17:19 +0800

    新增總和元件

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,122 @@ void main() {
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ThreeCounters(),
+      ),
+    );
+  }
+}
+
+class ThreeCounters extends StatefulWidget {
+  const ThreeCounters({
+    super.key,
+  });
+
+  @override
+  State<ThreeCounters> createState() => _ThreeCountersState();
+}
+
+class _ThreeCountersState extends State<ThreeCounters> {
+  var sums = [0, 0, 0];
+  @override
+  Widget build(BuildContext context) {
+    int sum = sums[0] + sums[1] + sums[2];
+    return ListView(
+      children: [
+        MyCounter(
+            num: 0,
+            onCounterChanged: (value) {
+              setState(() {
+                sums[0] = value;
+              });
+            }),
+        MyCounter(
+            num: 0,
+            onCounterChanged: (value) {
+              setState(() {
+                sums[1] = value;
+              });
+            }),
+        MyCounter(
+            num: 0,
+            onCounterChanged: (value) {
+              setState(() {
+                sums[2] = value;
+              });
+            }),
+        Text(
+          '三個數字的總和為: $sum',
+          style: TextStyle(fontSize: 32),
+        ),
+      ],
+    );
+  }
+}
+
+class MyCounter extends StatefulWidget {
+  // num 是初始值, onCounterChanged 是當數字改變時的 callback
+  final int num;
+  final Function(int) onCounterChanged;
+
+  const MyCounter({
+    super.key,
+    required this.num,
+    required this.onCounterChanged,
+  });
+
+  @override
+  MyCounterState createState() => MyCounterState();
+}
+
+class MyCounterState extends State<MyCounter> {
+  int myNumber = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    myNumber = widget.num;
+  }
 
   @override
   Widget build(BuildContext context) {
-    print('Hello World!');
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          // 測試 commit 1
-          // 測試 commit 2
-          child: Text('Hello World!', style: TextStyle(fontSize: 36)),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          myNumber.toString(),
+          style: TextStyle(
+              fontSize: 64,
+              color: myNumber >= 0 ? Colors.green : Colors.red,
+              fontWeight: FontWeight.bold),
         ),
-      ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  myNumber++;
+                  widget.onCounterChanged(myNumber);
+                });
+              },
+              icon: Icon(Icons.add, size: 64),
+            ),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  myNumber--;
+                  widget.onCounterChanged(myNumber);
+                });
+              },
+              icon: Icon(Icons.remove, size: 64),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
